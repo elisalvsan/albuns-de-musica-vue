c9ab3bdb7a5af4c4ada7fb4c2b5766eaf2147e9c
Elisa Andrade <elisalvsan@gmail.com>
2024-04-06 16:55:34 -0300

    modificando informações da HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+import AlbumCard from '../components/AlbumCard.vue';
+
+const albuns = ref([])
+const mensagem = ref(null)
+
+fetch('/data.json')
+  .then(response => response.json())
+  .then(data => albuns.value = data.albuns)
+  .catch(error => mensagem.value = error)
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <AlbumCard v-for="(album, index) in albuns" :album="album" />
   </main>
 </template>
